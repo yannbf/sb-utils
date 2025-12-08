5696f6ab887edd6152e25cf0497c62fb8641f9dd
Yann Braga <yannbf@gmail.com>
2025-12-08 10:17:32 +0100

    fix

--- libs/sb-utils/tsdown.config.ts
+++ libs/sb-utils/tsdown.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  exports: true,
   entry: ['src/bin.ts', 'src/commands/uninstall.ts'],
   banner: {
     js: '#!/usr/bin/env node',
